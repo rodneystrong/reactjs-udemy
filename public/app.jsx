03b064cbb3108f3257127f58026834dd7abc2f53
aleeex <akang2@gmail.com>
2017-01-16 19:14:22 -0800

    moved h1 from reactDOM to createClass render

--- public/app.jsx
+++ public/app.jsx
@@ -1,5 +1,20 @@
+//using uppercase is norm for components
+//react.createClass() is the most common method. takes
+//only one argument, an options object
+//all the render method expects is some JSX
+//code to be displayed to browser
+var Greeter = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+});
+
 //React.DOM is how we kick off react stuff
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  ,
   document.getElementById('app')
 );
