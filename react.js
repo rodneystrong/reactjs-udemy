c819f724952b192cf2753e79906ad8708444a45b
aleeex <akang2@gmail.com>
2017-01-16 18:29:44 -0800

    added express dependency + express settings + reareact.js server settings

---
+++ react.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+//create our app
+//
+var app = express();
+
+//tell it which folder we want to serve
+app.use(express.static('public'));
+
+//app.use lets you add functionality to your server
+
+//then start the server
+app.listen(3000, function() {
+  console.log('Express server is up on port 3000');
+});
